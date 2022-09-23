ea4e09ec3edb12d0d7e6329ed5c59bb14acdc22c
a.ganusina <a.ganusina@dicella.com>
2022-09-23 10:04:48 +0200

    feat: trying to add message return logic

---
+++ basic_app.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import requests
+from collections import namedtuple
+
+Book = namedtuple('Book', 'title,author')
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Library(Resource):
+
+    def __init__(self):
+        libraryList = []
+        book = Book('Harry Potter', 'Rowling')
+        libraryList.append(book)
+        book = Book('Martin Eden', 'Jack London')
+        libraryList.append(book)
+        book = Book('Alice in Wonderland', 'Lewis Carroll')
+        libraryList.append(book)
+
+
+    def list(self):
+        for book in self.libraryList:
+            print(f'Title: "{book.title}", author: "{book.author}"')
+        return "OK", 200
+
+
+    def add(self,title,author="William Shakespeare"):
+        book = Book(title, author)
+        self.libraryList.append(book)
+        return "OK", 200
+
+
+    def delete(self,title):
+        for book in self.libraryList.items():
+            if book.title == title:
+                del self.libraryList[book]
+            else:
+                print("The book isn't in library")
+                         
+        return "OK", 200
+
+        
+library = Library()
+
+app.add_url_rule('/library-app/list', view_func=library.list, methods=['GET'])
+app.add_url_rule('/library-app/add/<string:title>/<string:author>', view_func=library.add, methods=['POST'])
+app.add_url_rule('/library-app/delete/<string:title>', view_func=library.delete, methods=["DELETE"])
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5005, host='0.0.0.0')
--- commit_m_ver.py
+++ commit_m_ver.py
@@ -8,6 +8,7 @@ import git
 from collections import namedtuple
 from inspect import getmembers, isfunction, signature
 import importlib
+import itertools
 
 @contextlib.contextmanager
 def change_cwd(directory):
@@ -55,20 +56,17 @@ class CommitMessageVerification:
         for file_dir, file_type in self.changed_files:
             file_name = os.path.basename(file_dir)[:-3]
             path_to_file = os.path.join(cwd,file_dir)
-            # print(f'File name: {file_name}')
-            # print(f'File path: {path_to_file}')
 
-            spec = importlib.util.spec_from_file_location(file_name, path_to_file)
-            module = importlib.util.module_from_spec(spec)
-            modules.append(spec.loader.exec_module(module)) #spec.loader.exec_module(module)
+            try:
+                spec = importlib.util.spec_from_file_location(file_name, path_to_file)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+                modules.append(module)
+            except FileNotFoundError:
+                print(f"File {file_name} doesn't exist. Unable to install module.")
             # modules.append(importlib.import_module('.' + str(file_name), package=path_to_file))
 
-        return modules     
-        '''dir_to_module_name = [('\\').join([str(cwd),file_name_type[0]]) for file_name_type in self.changed_files]
-        print(f'pathes to modules: {dir_to_module_name}')
-        return [importlib.import_module(module_name, cwd) for module_name in dir_to_module_name]'''
-        # Replaces '/' with '.' in directory and removes ".py" at the end
-        #not the best way, should try something else
+        return modules
 
     def get_functions_signature_dict(self, list_of_functions):
         func_sign = {}
@@ -76,22 +74,64 @@ class CommitMessageVerification:
             func_signature = signature(func_obj)
             func_sign[func_name] = func_signature
         return func_sign
+    
+    def cls_in_module(self, module):
+        md = module.__dict__
+        return [
+            md[c] for c in md if (
+                isinstance(md[c], type) and md[c].__module__ == module.__name__
+            )
+        ]
 
     def get_cls_func_sign(self, modules_imported):
         # Get list of classes
-        clsmembers = [inspect.getmembers(module, inspect.isclass) for module in modules_imported]
+        # clsmembers = [inspect.getmembers(module, inspect.isclass) for module in modules_imported]
+        clsmembers = [self.cls_in_module(module) for module in modules_imported]
+        clsmembers = list(itertools.chain(*clsmembers))
         print(f'Repository classes: {clsmembers}\n')
 
         # Get functions for each class
         class_dict = {}
-        for cls_name, cls_obj in clsmembers:
+        for cls_obj in clsmembers:
             class_functions = getmembers(cls_obj, isfunction) # inspect.isfunction ?
-            print(f'Functions of class named {cls_name}: {class_functions}\n')
+            # print(f'Functions of class {cls_obj}: {class_functions}\n')
 
             func_signature_dict = self.get_functions_signature_dict(class_functions)
-            print(f'Functions with signatures: {func_signature_dict}\n')
+            # print(f'Functions with signatures: {func_signature_dict}\n')
 
-            class_dict[cls_name] = func_signature_dict
+            class_dict[cls_obj] = func_signature_dict
+        return class_dict
+
+    def compare_directories(self, cls_after_changes, cls_before_changes):
+        result = "FIX"
+        for cls_obj, func_list in cls_after_changes.items():
+            if cls_obj in cls_before_changes:
+                func_before_changes = cls_before_changes[cls_obj]
+                for func_name, func_sign in func_list:
+                    if func_name in func_before_changes:
+                        func_sign_before_changes = func_before_changes[func_name]
+                        
+                        for aft, bef in zip(func_sign.parameters,func_sign_before_changes.parameters):
+                            if len(aft) > len(bef):
+                                result = "FEAT"
+                                new_params = list(set(aft) - set(bef))
+                                for param in new_params:
+                                    if '=' not in param:
+                                        # Added argument is not optional
+                                        return "MAJOR"
+                    else:
+                        # the method was added after the change
+                        result = "FEAT"
+            else:
+                # the class was added after the change
+                result = "FEAT"
+
+        for cls_obj, func_list in cls_before_changes.items():
+            if cls_obj not in cls_after_changes:
+                # the class is missing after the changes
+                return "MAJOR"
+
+        return result
 
     
     def get_message(self):
@@ -101,22 +141,29 @@ class CommitMessageVerification:
 
             # Get staged changed files status
             self.changed_files = self.get_changed_files()
-            print(f'Changed files: {self.changed_files}\n')
+            # print(f'Changed files: {self.changed_files}\n')
 
             modules_imported = self.get_modules_to_import()
-            print(f'Modules imported: {modules_imported}\n')
+            # print(f'Modules imported: {modules_imported}\n')
 
             cls_after_changes = self.get_cls_func_sign(modules_imported)
 
             with GitStash(self.repository, self.changed_files):
-                cls_before_changes = []
+                modules_imported = self.get_modules_to_import()
+                cls_before_changes = self.get_cls_func_sign(modules_imported)
 
-                print(f'Changed files after git stash push: {self.get_changed_files()}\n')
+                # print(f'Changed files after git stash push: {self.get_changed_files()}\n')
             
-            print(f'Changed files after git stash pop: {self.get_changed_files()}\n')
+            # print(f'Changed files after git stash pop: {self.get_changed_files()}\n')
 
-            # class_dict = self.get_cls_func_sign()
-            # print(f'Classes with functions with signatures: {class_dict}\n')
+            print(f'Classes with signatures after changes: {cls_after_changes}\n')
+
+            print(f'Classes with signatures before changes: {cls_before_changes}\n')
+
+            print(f'RESULT:    {self.compare_directories(cls_after_changes, cls_before_changes)}')
+
+            
+
 
 
 if __name__ == '__main__':
